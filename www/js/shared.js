5f57484e185d843e663f247077fad0268f565255
Jan Chalupa <jan.chalupa@actum.cz>
2016-11-29 12:28:06 +0100

    no message

--- www/js/shared.js
+++ www/js/shared.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".side-panel-placeholder").load("partial/side-panel.html", function() {
+    $(".side-panel-placeholder").load("http://www.honzachalupa.cz/dev/partial/side-panel.html", function() {
         $("h2 li").each(function() {
             console.log("x");
             $(this).css("border-color", "rgb(" + rndRange(0, 255) + ", " + rndRange(0, 255) + ", " + rndRange(0, 255) + ")");
